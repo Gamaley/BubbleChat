365ae1e652f98e6958110b8d31720720cb6b9fc1
Gamaley <gamaley@me.com>
2016-01-06 10:18:27 +0200

    Initial Commit

---
+++ TableViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewTest
+//
+//  Created by Vladyslav on 06.01.16.
+//  Copyright © 2016 Vladyslav Gamalii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewTest
+//
+//  Created by Vladyslav on 06.01.16.
+//  Copyright © 2016 Vladyslav Gamalii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
